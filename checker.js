ce57af37bd9f92d80d529e53e839fb84c91a268c
Wilfred Hughes <me@wilfred.me.uk>
2013-08-04 19:51:14 +0100

    fleshing out the basic JS

--- checker.js
+++ checker.js
@@ -1,1 +1,46 @@
-console.log('hello world');
+(function() {
+    "use strict";
+
+    var URLS = {
+        alcohol: [],
+        dating: [{
+            url: "https://www.pof.com/",
+            name: "Plenty Of Fish"
+        }, {
+            url: "http://www.match.com/",
+            name: "match.com"
+        }, {
+            url: "https://www.zoosk.com/",
+            name: "Zoosk"
+        }, {
+            url: "http://www.meetic.co.uk/",
+            name: "meetic"
+        }],
+        drugs: [],
+        fileSharing: [],
+        gambling: [],
+        gaming: [],
+        hacking: [],
+        politicallyControversial: [],
+        pornography: [],
+        sexuality: [],
+        socialNetworks: [],
+        weaponsAndViolence: [],
+        tobacco: [],
+    }
+
+    var results = {};
+
+    function tryRobotsTxt(category, url, name) {
+        var robotsUrl = url + "robots.txt"; 
+        
+        $.get(robotsUrl, function() {
+                console.log('success!');
+        }, "text")
+            .fail(function() {
+                console.log('failed :(');
+            });
+    }
+
+    tryRobotsTxt("dating", "https://www.pof.com/", "POF");
+})();
