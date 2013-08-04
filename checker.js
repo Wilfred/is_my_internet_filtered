18e14f1e5a8dbd32e45c81012b3b106d1922f6d1
Wilfred Hughes <me@wilfred.me.uk>
2013-08-04 20:32:52 +0100

    adding proof-of-concept JS

--- checker.js
+++ checker.js
@@ -4,16 +4,13 @@
     var URLS = {
         alcohol: [],
         dating: [{
-            url: "https://www.pof.com/",
-            name: "Plenty Of Fish"
-        }, {
-            url: "http://www.match.com/",
+            url: "http://www.match.com/favicon.ico",
             name: "match.com"
         }, {
-            url: "https://www.zoosk.com/",
+            url: "https://www.zoosk.com/favicon.ico",
             name: "Zoosk"
         }, {
-            url: "http://www.meetic.co.uk/",
+            url: "http://www.meetic.co.uk/favicon.ico",
             name: "meetic"
         }],
         drugs: [],
@@ -31,16 +28,21 @@
 
     var results = {};
 
-    function tryRobotsTxt(category, url, name) {
-        var robotsUrl = url + "robots.txt"; 
-        
-        $.get(robotsUrl, function() {
-                console.log('success!');
-        }, "text")
-            .fail(function() {
-                console.log('failed :(');
-            });
+    function checkImage(url) {
+        var img = new Image();
+        img.onload = function() {
+            console.log('all good for ' + url)
+        };
+        img.onerror = function() {
+            console.log('failed for ' + url);
+        };
+
+        img.src = url;
     }
 
-    tryRobotsTxt("dating", "https://www.pof.com/", "POF");
+
+    $.each(URLS.dating, function(i, site) {
+        checkImage(site.url);
+    });
+
 })();
